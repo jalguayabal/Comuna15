6c4e8d03fd09d5fad116db812ccce8141e4d66bd
jalguayabal <comunicacionesjalguayabal@gmail.com>
2019-07-30 21:23:56 -0500

    version 1.4

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +13,10 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  HideNavbar(){
+    $('#nbar').click();
   }
 
 }
